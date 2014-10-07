3a6077498f9e12740febe04f9ec88d7569c911f2
danm <writetodan@yahoo.com>
2014-10-07 23:29:57 +0100

    added ability for single scores and tallies (not buckets); added aug + oct 2013

--- show_average.rb
+++ show_average.rb
@@ -1,16 +1,21 @@
 def show_average(date, buckets)
   total = 0
   count = 0
-  buckets.each do |range, block_and_singles|
+  buckets.each do |score, tally|
+    range = score.is_a?(Range) ? score : score..score
     lower, upper = range.first, range.last
     bucket_middle = (lower + upper) / 2
-    blocks, singles = block_and_singles
-    bucket_count = blocks * 9 + singles
+    if tally.is_a? Array 
+      blocks, singles = tally
+      bucket_count = (blocks * 9) + singles
+    else
+      bucket_count = tally
+    end
     count += bucket_count
     total += bucket_count * bucket_middle
   end
 
   average = total.to_f / count
 
-  puts "#{date}: average #{average.round(2)} of #{count} scores"
+  puts "#{date}: average #{average.round(1)} of #{count} scores"
 end
