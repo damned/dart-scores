439360f68c23d66d6fb13f2394df4c128676ced2
danm <writetodan@yahoo.com>
2014-12-26 12:36:05 +0000

    @damned complete scores.txt (had to copy to external and use MSC); parsing scores.txt

--- scores_from_scorer.rb
+++ scores_from_scorer.rb
@@ -10,22 +10,49 @@
 }
 
 class Scores
-  attr_reader :buckets, :date
+  attr_reader :buckets, :time
   def initialize
-    @buckets = { 50..54 => 7 }
-    @date = Time.now
+    @buckets = init_buckets
+    @time = Time.now
+  end
+  def add(score, time)
+    buckets.each do |bucket, tally|
+      if bucket.include? score
+        buckets[bucket] = tally + 1
+      end
+    end
+    @time = time if time < @time
+  end
+  def to_s
+    buckets.to_s
+  end
+
+  private
+
+  def init_buckets(size = 5, count = 20)
+    starts = (0..(count-1)).map {|i| i * 5}
+    ranges = starts.map {|start| start..(start + size - 1) }
+    buckets = {}
+    ranges.each {|range| buckets[range] = 0 }
+    buckets
   end
 end
 
 class ScorerLogParser
-  def parse_from_csv(csv)
+  require 'time'
+  def parse_from_csv(csvlines)
     scores = Scores.new 
+    csvlines.each { |line|
+      score_s, time_s = line.split ','
+      scores.add(score_s.to_i, Time.parse(time_s)) unless score_s.empty?
+    }
     scores
   end
 end
 
 require_relative 'show_average'
 
-scores = ScorerLogParser.new.parse_from_csv('')
-show_average(scores.date.to_s, scores.buckets)
+log = File.readlines 'from-scorer/scores.txt'
+scores = ScorerLogParser.new.parse_from_csv log
+show_average(scores.time.to_s, scores.buckets)
 
